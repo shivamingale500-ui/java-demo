90c6b20035f14530146fc45f857db958d97094f4
shivamingale500-ui <shivamingale500@gmail.com>
2026-08-20 17:13:36 +0000

    Added

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,42 +1,33 @@
 package com.example;
-
+ 
 import com.sun.net.httpserver.HttpServer;
-
 import java.io.IOException;
-
 import java.io.OutputStream;
-
 import java.net.InetSocketAddress;
-                    "this is integration of docker"
+ 
 public class App {
-
+ 
     public static void main(String[] args) throws IOException {
-
+ 
         HttpServer server =
-
                 HttpServer.create(new InetSocketAddress(8082), 0);
-
+ 
         server.createContext("/", exchange -> {
-
+ 
             String response =
-
                     "Hello from Java application running inside Docker!";
-
+ 
             exchange.sendResponseHeaders(200, response.length());
-
+ 
             OutputStream outputStream = exchange.getResponseBody();
-
+ 
             outputStream.write(response.getBytes());
-
+ 
             outputStream.close();
-
         });
-
+ 
         server.start();
-
+ 
         System.out.println("Server started on port 8082");
-
     }
-
 }
-
